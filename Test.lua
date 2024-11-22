3477144c9d643d2f5ea4418457271093c243567d
I4MMO <126089820+I4MMO@users.noreply.github.com>
2024-11-22 23:52:19 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,21 +1,5 @@
-writefile("BYPASS.txt", [[
-
 XYZ = function(Key)
     print("KEY:", Key)
     
     return true;
 end;
-
-]])
-
-
-
-loadstring(readfile("BYPASS.txt"))()
-
-local Returned = XYZ("Nigga")
-print("RETURNED:", Returned, "Anti-Cheat Bypassed")
-
-
-
-
-print("Load Detazy premium...")
